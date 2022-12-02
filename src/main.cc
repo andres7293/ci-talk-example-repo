efe28604a545d642708561ac32cf0085494e0423
Andres <andres7293@gmail.com>
2022-12-02 12:10:34 +0000

    Create a lists of points and print them using LinkedList class

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,32 @@
 #include <iostream>
+#include "LinkedList.H"
 using namespace std;
 
-int main(void) { return 0; }
+class Point {
+    public:
+        Point(float x, float y): x(x), y(y) {};
+        float getX() { return this->x; }
+        float getY() { return this->y; }
+    private:
+        float x, y;
+};
+
+int main(void) { 
+    LinkedList<Point> point_list;
+    for (int i = 0; i < 1000; i++) {
+        float coordinate = static_cast<float>(i);
+        auto point = Point{coordinate, coordinate};
+        point_list.add(point);
+    }
+
+    auto print_points = [](Point p) {
+        std::cout << "Point{"
+            << "x=" << p.getX()
+            << ","
+            << "y=" << p.getY()
+            << "}"
+            << std::endl;
+    };
+
+    point_list.iterate(print_points);
+}
